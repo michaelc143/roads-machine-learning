df4faa9140bd8ba8fad40b4b21e21a951a9059df
Michael Corbishley <mcorbishley@wisc.edu>
2023-08-13 12:59:23 -0500

    higher score

--- roads.py
+++ roads.py
@@ -79,7 +79,7 @@ model.compile(optimizer=tf.keras.optimizers.Nadam(learning_rate=0.001), loss='bi
 lr_scheduler = LearningRateScheduler(lr_schedule)
 
 # Hyper param that makes model stop learning once val_loss has stopped decreasing.
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=15, restore_best_weights=True)
 
 # Step 7: Training
 batch_size = 8
